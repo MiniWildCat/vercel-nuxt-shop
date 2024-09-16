c533fff825537d24a20ae49f890804eca7508ef1
xlf <2276974147@qq.com>
2024-09-16 22:48:02 +0800

    feat:修改一下

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -1,6 +1,6 @@
-export { default as Loading } from '../..\\components\\loading.vue'
-export { default as NuxtLogo } from '../..\\components\\NuxtLogo.vue'
-export { default as Tutorial } from '../..\\components\\Tutorial.vue'
+export const Loading = () => import('../..\\components\\loading.vue' /* webpackChunkName: "components/loading" */).then(c => wrapFunctional(c.default || c))
+export const NuxtLogo = () => import('../..\\components\\NuxtLogo.vue' /* webpackChunkName: "components/nuxt-logo" */).then(c => wrapFunctional(c.default || c))
+export const Tutorial = () => import('../..\\components\\Tutorial.vue' /* webpackChunkName: "components/tutorial" */).then(c => wrapFunctional(c.default || c))
 
 // nuxt/nuxt.js#8607
 function wrapFunctional(options) {
