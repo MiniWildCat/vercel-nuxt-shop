d5b6eb117c874722ca9ae04e0f4aa0c8bd951a26
xlf <2276974147@qq.com>
2024-09-16 20:46:12 +0800

    feat:初始化nuxt2

---
+++ .nuxt/router.js
@@ -0,0 +1,108 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { normalizeURL, decode } from 'ufo'
+import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
+
+const _7871ee86 = () => interopDefault(import('..\\pages\\demo\\index.vue' /* webpackChunkName: "pages/demo/index" */))
+const _cad6fbe6 = () => interopDefault(import('..\\pages\\user.vue' /* webpackChunkName: "pages/user" */))
+const _8f124c56 = () => interopDefault(import('..\\pages\\user\\index.vue' /* webpackChunkName: "pages/user/index" */))
+const _662b4d7d = () => interopDefault(import('..\\pages\\user\\_id.vue' /* webpackChunkName: "pages/user/_id" */))
+const _38d79251 = () => interopDefault(import('..\\pages\\demo\\one\\index.vue' /* webpackChunkName: "pages/demo/one/index" */))
+const _d20780ee = () => interopDefault(import('..\\pages\\demo\\three.vue' /* webpackChunkName: "pages/demo/three" */))
+const _5d6bcdf7 = () => interopDefault(import('..\\pages\\demo\\two.vue' /* webpackChunkName: "pages/demo/two" */))
+const _1f305b3b = () => interopDefault(import('..\\pages\\demo2\\list.vue' /* webpackChunkName: "pages/demo2/list" */))
+const _721a94f2 = () => interopDefault(import('..\\pages\\demo2\\list2.vue' /* webpackChunkName: "pages/demo2/list2" */))
+const _71fe65f0 = () => interopDefault(import('..\\pages\\demo2\\list3.vue' /* webpackChunkName: "pages/demo2/list3" */))
+const _71e236ee = () => interopDefault(import('..\\pages\\demo2\\list4.vue' /* webpackChunkName: "pages/demo2/list4" */))
+const _763d8680 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
+const _6f17f4f9 = () => interopDefault(import('..\\pages\\demo\\one\\_id.vue' /* webpackChunkName: "pages/demo/one/_id" */))
+
+const emptyFn = () => {}
+
+Vue.use(Router)
+
+export const routerOptions = {
+  mode: 'history',
+  base: '/',
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
+
+  routes: [{
+    path: "/demo",
+    component: _7871ee86,
+    name: "demo"
+  }, {
+    path: "/user",
+    component: _cad6fbe6,
+    children: [{
+      path: "",
+      component: _8f124c56,
+      name: "user"
+    }, {
+      path: ":id",
+      component: _662b4d7d,
+      name: "user-id"
+    }]
+  }, {
+    path: "/demo/one",
+    component: _38d79251,
+    name: "demo-one"
+  }, {
+    path: "/demo/three",
+    component: _d20780ee,
+    name: "demo-three"
+  }, {
+    path: "/demo/two",
+    component: _5d6bcdf7,
+    name: "demo-two"
+  }, {
+    path: "/demo2/list",
+    component: _1f305b3b,
+    name: "demo2-list"
+  }, {
+    path: "/demo2/list2",
+    component: _721a94f2,
+    name: "demo2-list2"
+  }, {
+    path: "/demo2/list3",
+    component: _71fe65f0,
+    name: "demo2-list3"
+  }, {
+    path: "/demo2/list4",
+    component: _71e236ee,
+    name: "demo2-list4"
+  }, {
+    path: "/",
+    component: _763d8680,
+    name: "index"
+  }, {
+    path: "/demo/one/:id",
+    component: _6f17f4f9,
+    name: "demo-one-id"
+  }],
+
+  fallback: false
+}
+
+export function createRouter (ssrContext, config) {
+  const base = (config._app && config._app.basePath) || routerOptions.base
+  const router = new Router({ ...routerOptions, base  })
+
+  // TODO: remove in Nuxt 3
+  const originalPush = router.push
+  router.push = function push (location, onComplete = emptyFn, onAbort) {
+    return originalPush.call(this, location, onComplete, onAbort)
+  }
+
+  const resolve = router.resolve.bind(router)
+  router.resolve = (to, current, append) => {
+    if (typeof to === 'string') {
+      to = normalizeURL(to)
+    }
+    return resolve(to, current, append)
+  }
+
+  return router
+}
